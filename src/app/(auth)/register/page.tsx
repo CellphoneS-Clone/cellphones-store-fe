5111a98e896d3b6f037f54a30618661065e60727
Trần Trân Lộc <tran26122003@gmail.com>
2025-06-17 21:03:15 +0700

    Done UI/UX Login and Register

--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -1,10 +1,35 @@
+import RegisterForm from '@/components/auth/RegisterForm';
+
+export const metadata = {
+    title: 'Smember | Tri ân khách hàng thân thiết - Tích điểm đổi quà',
+    description:
+        'Quà tặng tri ân khách hàng thân thiết - VIP membership của CellphoneS. Tích điểm Smember săn voucher, mức chiết khấu và ưu đãi sinh nhật hấp dẫn.',
+    keywords: 'Smember,Tri ân khách hàng,Khách hàng thân thiết,Tích điểm đổi quà',
+    robots: 'index, follow',
+    canonical: 'https://smember.com.vn',
+    og: {
+        title: 'Smember | Tri ân khách hàng thân thiết - Tích điểm đổi quà',
+        description:
+            'Quà tặng tri ân khách hàng thân thiết - VIP membership của CellphoneS. Tích điểm Smember săn voucher, mức chiết khấu và ưu đãi sinh nhật hấp dẫn.',
+        url: 'https://smember.com.vn',
+        site_name: 'Web Smember',
+        locale: 'vi_VN',
+        type: 'website',
+    },
+    twitter: {
+        card: 'summary',
+        title: 'Smember | Tri ân khách hàng thân thiết - Tích điểm đổi quà',
+        description:
+            'Quà tặng tri ân khách hàng thân thiết - VIP membership của CellphoneS. Tích điểm Smember săn voucher, mức chiết khấu và ưu đãi sinh nhật hấp dẫn.',
+    },
+};
+
 export default function RegisterPage() {
     return (
-        <div className="min-h-screen flex items-center justify-center">
-            <div className="bg-white p-8 rounded-lg shadow-md w-96">
-                <h1 className="text-2xl font-bold mb-6 text-center">Register</h1>
-                {/* Add your registration form here */}
+        <div className="min-h-screen w-full">
+            <div className="w-full px-4 py-6 tablet:px-8 tablet:py-16">
+                <RegisterForm />
             </div>
         </div>
     );
-} 
+}
